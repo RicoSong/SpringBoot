936af377bc2ddb14975143b9c0117f5690f018b0
SpringBoot <songrui12138@gmail.com>
2019-11-02 16:48:43 +0800

    Update BootStrap

--- src/main/java/com/example/demo/Controller/Hander.java
+++ src/main/java/com/example/demo/Controller/Hander.java
@@ -14,8 +14,11 @@ import java.util.List;
 public class Hander {
     @Autowired
     private Test_Service test_service;
+
     @RequestMapping("/select")
-    public List<Data> select(){
-        return test_service.select();
+    public List<Data> select() {
+      /*  List<Data> select = test_service.select();
+        return null;*/
+       return test_service.select();
     }
 }
