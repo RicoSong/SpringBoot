58732ef94612c9e3d63ef990159fc313e4415b37
SpringBoot <songrui12138@gmail.com>
2019-11-09 22:54:52 +0800

    thymeleaf cache

--- src/main/java/com/example/demo/Controller/Handler.java
+++ src/main/java/com/example/demo/Controller/Handler.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
-
 @Controller
 public class Handler {
     @Autowired
--- src/main/java/com/example/demo/Service/Test_Service.java
+++ src/main/java/com/example/demo/Service/Test_Service.java
@@ -2,7 +2,6 @@ package com.example.demo.Service;
 
 import com.example.demo.dao.Mapper;
 import com.example.demo.pojo.Blog;
-import com.example.demo.pojo.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
